1db86c4d95ce474703287bf4308343b23e5bd116
OiCMudkips <victor_zhou1995@hotmail.ca>
2020-06-26 13:08:34 -0700

    update populate script for s3

--- populate_db.py
+++ populate_db.py
@@ -1,20 +1,27 @@
+"""Populates the DB with image paths on S3.
+Depends on a AWS credentials file with a profile called "s3.
+"""
+import boto3
 import os
 import sqlite3
 import sys
 
 def main():
-    db_file = sys.argv[1]
-    photos_directory = sys.argv[2]
+    db_file, bucket_name, bucket_path = sys.argv[1:]
 
     db_conn = sqlite3.connect(db_file)
     db_cursor = db_conn.cursor()
 
-    for file_path in os.listdir(photos_directory):
-        real_path = os.path.join(photos_directory, file_path)
-        db_cursor.execute('INSERT INTO image (path) VALUES (?)', (file_path,))
+    s3_client = boto3.Session(profile_name='s3').client('s3')
+
+    for resource in s3_client.list_objects(Bucket=bucket_name, Prefix=bucket_path)['Contents']:
+        bucket_path = resource['Key']
+        if bucket_path.endswith('/'):
+            continue
+        db_cursor.execute('INSERT INTO image (bucket, s3_path) VALUES (?,?)', (bucket_name, bucket_path))
 
     db_conn.commit()
-    db_conn.close()    
+    db_conn.close()
 
 if __name__ == '__main__':
     main()
