17292b26ae2ec4475c08a7134b6b7afd59c6ea2e
OiCMudkips <victor_zhou1995@hotmail.ca>
2020-06-26 13:08:51 -0700

    update create script for s3

--- create_db.py
+++ create_db.py
@@ -5,7 +5,7 @@ def main():
     db_file = sys.argv[1]
 
     db_cursor = sqlite3.connect(db_file).cursor()
-    db_cursor.execute('CREATE TABLE image (id INTEGER PRIMARY KEY AUTOINCREMENT, path TEXT, caption TEXT, uploaded INTEGER DEFAULT 0);')
+    db_cursor.execute('CREATE TABLE image (id INTEGER PRIMARY KEY AUTOINCREMENT, bucket TEXT, s3_path TEXT, caption TEXT, uploaded INTEGER DEFAULT 0);')
 
 if __name__ == '__main__':
     main()
